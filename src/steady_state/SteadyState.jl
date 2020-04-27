1f5e20d33440acf49e209d6347ff5c09db953ea3
MichelJuillard <michel.juillard@mjui.fr>
2020-04-27 13:16:09 +0200

    set steady state to 0 in absence of steady state model (for now)

--- src/steady_state/SteadyState.jl
+++ src/steady_state/SteadyState.jl
@@ -3,10 +3,11 @@ function steady_state!(context)
     model = context.models[1]
     work = context.work
     steadystatemodule = model.steady_state! 
+    results = context.results.model_results[1]
+    fill!(results.exogenous_steady_state, 0.0)
     if isnothing(steadystatemodule)
+        fill!(results.endogenous_steady_state, 0.0)    
     else
-        results = context.results.model_results[1]
-        fill!(results.exogenous_steady_state, 0.0)
         steadystatemodule.steady_state!(results.endogenous_steady_state,
                                         results.exogenous_steady_state,
                                         work.params)
