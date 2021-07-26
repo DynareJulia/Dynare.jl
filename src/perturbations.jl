1ac2b03ffa654c0120141e2e33bafc433aba707c
MichelJuillard <michel.juillard@mjui.fr>
2021-07-26 13:59:55 +0200

    use compute_variance() from LinearRationalExpectations

--- src/perturbations.jl
+++ src/perturbations.jl
@@ -94,7 +94,6 @@ function stoch_simul_core!(context::Context, ws::PeriodJacobianWs, options::Stoc
     if options.display
         display_stoch_simul(context)
     end
-    compute_variance!(context)
     if (periods = options.periods) > 0
         steadystate = results.trends.endogenous_steady_state
         linear_trend = results.trends.endogenous_linear_trend
@@ -130,6 +129,7 @@ function stoch_simul_core!(context::Context, ws::PeriodJacobianWs, options::Stoc
 end
 
 function check!(context::Context, field::Dict{String, Any})
+    Nothing
 end
 
 function compute_stoch_simul!(context::Context, ws::PeriodJacobianWs,
@@ -140,23 +140,28 @@ function compute_stoch_simul!(context::Context, ws::PeriodJacobianWs,
     endogenous = results.trends.endogenous_steady_state
     exogenous = results.trends.exogenous_steady_state
     fill!(exogenous, 0.0)
-    compute_first_order_solution!(results.linearrationalexpectations,
+    compute_first_order_solution!(context,
                                   endogenous, exogenous, endogenous,
                                   params, model, ws, options)
 end
 
 function compute_first_order_solution!(
-    results::LinearRationalExpectationsResults,
+    context::Context,
     endogenous::AbstractVector{Float64},
     exogenous::AbstractVector{Float64},
     steadystate::AbstractVector{Float64},
     params::AbstractVector{Float64},
-    model::Model, ws::PeriodJacobianWs, options::StochSimulOptions)
+    model::Model,
+    ws::PeriodJacobianWs,
+    options::StochSimulOptions)
 
     # abbreviations
     LRE = LinearRationalExpectations
     LREWs = LinearRationalExpectationsWs
 
+    results = context.results.model_results[1]
+    LRE_results = results.linearrationalexpectations
+            
     get_jacobian!(ws, params, endogenous, exogenous, steadystate,
                   model, 2)
     algo = options.dr_algo
@@ -175,89 +180,21 @@ function compute_first_order_solution!(
     else
         LRE.get_abc!(wsLRE, ws.jacobian)
     end
-    LRE.first_order_solver!(results,
+    LRE.first_order_solver!(LRE_results,
                             algo,
                             ws.jacobian,
                             options.LRE_options,
                             wsLRE)
+    lre_variance_ws = LRE.VarianceWs(model.endogenous_nbr,
+                                     model.n_bkwrd + model.n_both,
+                                     model.exogenous_nbr,
+                                     wsLRE)
+    compute_variance!(results.endogenous_variance,
+                      LRE_results,
+                      model.Sigma_e,
+                      lre_variance_ws)
 end
 
-function compute_variance!(context::Context)
-    m = context.models[1]
-    results = context.results.model_results[1]
-    Σy = results.endogenous_variance
-    A = results.linearrationalexpectations.gs1
-    B1 = zeros(m.n_states, m.exogenous_nbr)
-    g1_1 = results.linearrationalexpectations.g1_1
-    g1_2 = results.linearrationalexpectations.g1_2
-    vr1 = view(g1_2, m.i_bkwrd_b, :)
-    B1 .= vr1
-    ws = LyapdWs(m.n_states::Int64)
-    Σ = zeros(m.n_states, m.n_states)
-    tmp = zeros(m.n_states, m.exogenous_nbr)
-    mul!(tmp, B1, m.Sigma_e)
-    B = zeros(m.n_states, m.n_states)
-    mul!(B, tmp, B1')
-    extended_lyapd!(Σ, A, B, ws)
-    stationary_variables = results.stationary_variables
-    fill!(stationary_variables, true)
-    state_stationary_variables = view(stationary_variables, m.i_bkwrd_b)
-    nonstate_stationary_variables = view(stationary_variables, m.i_non_states)
-    if is_stationary(ws)
-        state_stationary_nbr = m.n_states 
-        nonstate_stationary_nbr = m.endogenous_nbr - m.n_states
-    else
-        fill!(Σy, NaN)
-        state_stationary_variables .= .!ws.nonstationary_variables
-        state_stationary_nbr = count(state_stationary_variables)
-        vr3 = view(results.linearrationalexpectations.g1_1, m.i_non_states, :)
-        for i = 1:(m.endogenous_nbr - m.n_states)
-            for j = 1:m.n_states
-                if ws.nonstationary_variables[j] && abs(vr3[i, j]) > 1e-10
-                    nonstate_stationary_variables[j] = false
-                    break
-                end
-            end
-        end
-        nonstate_stationary_nbr = count(nonstate_stationary_variables)
-    end
-    # state / state
-    stationary_nbr = state_stationary_nbr + nonstate_stationary_nbr
-    A2 = zeros(nonstate_stationary_nbr, state_stationary_nbr)
-    vtmp = view(results.linearrationalexpectations.g1_1, m.i_non_states, :)
-    A2 .= view(vtmp, nonstate_stationary_variables, state_stationary_variables)
-    vtmp = view(Σy, m.i_bkwrd_b, m.i_bkwrd_b)
-    vΣy = view(vtmp, state_stationary_variables, state_stationary_variables)
-    vΣ = view(Σ, state_stationary_variables, state_stationary_variables) 
-    vΣy .= vΣ
-    # endogenous / nonstate
-    n_non_states = m.endogenous_nbr - m.n_states
-    B2 = zeros(nonstate_stationary_nbr, m.exogenous_nbr)
-    vtmp = view(results.linearrationalexpectations.g1_2, m.i_non_states, :)
-    vr2 = view(vtmp, nonstate_stationary_variables, :)
-    B2 .= vr2
-    Σ = zeros(stationary_nbr, nonstate_stationary_nbr)
-    vg1 = view(results.linearrationalexpectations.g1_1, stationary_variables, state_stationary_variables)
-    tmp1 = zeros(stationary_nbr, state_stationary_nbr)
-    mul!(tmp1, vg1, vΣy)
-    vg1 = view(results.linearrationalexpectations.g1_1, m.i_non_states, :)
-    vg11 = view(vg1, nonstate_stationary_variables, state_stationary_variables)
-    mul!(Σ, tmp1, transpose(vg11))
-    tmp2 = zeros(stationary_nbr, m.exogenous_nbr)
-    vg2 = view(results.linearrationalexpectations.g1_2, stationary_variables, :)
-    mul!(tmp2, vg2, m.Sigma_e)
-    mul!(Σ, tmp2, transpose(B2), 1.0, 1.0)
-    vtmp = view(Σy, :, m.i_non_states) 
-    vΣy = view(vtmp, stationary_variables, nonstate_stationary_variables)
-    vΣy .= Σ
-    # nonstate / state
-    vtmp1 = view(Σy, m.i_non_states, m.i_bkwrd_b)
-    vΣy1 = view(vtmp1, nonstate_stationary_variables, state_stationary_variables)
-    vtmp2 = view(Σy, m.i_bkwrd_b, m.i_non_states)
-    vΣy2 = view(vtmp2, state_stationary_variables, nonstate_stationary_variables)
-    vΣy1 .= transpose(vΣy2)
-end
-    
 function simul_first_order!(results::AbstractMatrix{Float64}, initial_values::AbstractVector{Float64}, x::AbstractVecOrMat{Float64}, c::AbstractVector{Float64}, A::StridedVecOrMat{Float64}, B::StridedVecOrMat{Float64}, periods::Int64)
     oldthreadnbr = BLAS.get_num_threads()
     BLAS.set_num_threads(1)
