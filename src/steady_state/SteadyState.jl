eae47644696366e5609f498c8532de4d7b96dff0
MichelJuillard <michel.juillard@mjui.fr>
2021-12-07 17:00:16 +0100

    fix steady. Passes tests

--- src/steady_state/SteadyState.jl
+++ src/steady_state/SteadyState.jl
@@ -86,10 +86,8 @@ function steady!(context::Context, field::Dict{String,Any})
         results = context.results.model_results[1]
         # will fail if missing values are encountered
         x0 = Float64.(vec(view(context.work.initval_endogenous, 1, :)))
-        @show context.work.initval_exogenous[94]
         copy!(results.trends.exogenous_steady_state,
               Float64.(vec(view(context.work.initval_exogenous, 1, :))))
-        @show results.trends.exogenous_steady_state[94]
         solve_steady_state!(context, x0)
     end
     if options.display
@@ -175,20 +173,40 @@ function solve_steady_state!(context::Context, x0::AbstractVector{Float64})
     results = context.results.model_results[1]
     exogenous = results.trends.exogenous_steady_state
 
-    residual_function(x::AbstractVector{Float64}) =
-        get_static_residuals!(ws, w.params, x, exogenous, m)
-
-    if count(!iszero, get_static_jacobian!(ws, w.params, x, exogenous, m)) < 0.1*m.endogenous_nbr*m.endogenous_nbr
-        jacobian_function(x::AbstractVector{Float64}) =
-            sparse_static_jacobian(ws, w.params, x, exogenous, m)
+    if count(!iszero, get_static_jacobian!(ws, w.params, x0, exogenous, m)) < 0.1*m.endogenous_nbr*m.endogenous_nbr
+        function J1!(A, x::AbstractVector{Float64})
+            A = sparse_static_jacobian(ws, w.params, x, exogenous, m)
+        end
+        A0 = sparse_static_jacobian(ws, w.params, x0, exogenous, m)
+        solve_steady_state_core!(context, x0, J1!, A0)
     else
-        jacobian_function(x::AbstractVector{Float64}) =
-            get_static_jacobian!(ws, w.params, x, exogenous, m)
+        function J2!(A::AbstractMatrix{Float64}, x::AbstractVector{Float64})
+            A .= get_static_jacobian!(ws, w.params, x, exogenous, m)
+        end
+        A0 = Matrix{Float64}(undef, m.endogenous_nbr, m.endogenous_nbr)
+        J2!(A0, x0)
+        solve_steady_state_core!(context, x0, J2!, A0)
     end
-        
-    df = OnceDifferentiable(f!, J!, vec(y0), residuals, A0)
-    result = nlsolve(residual_function, jacobian_function, x0::Vector{Float64},
-                     show_trace=true)
+end
+
+function solve_steady_state_core!(context, x0, J!, A0)
+    
+    ws = StaticWs(context)
+    m = context.models[1]
+    w = context.work
+    results = context.results.model_results[1]
+    exogenous = results.trends.exogenous_steady_state
+
+    function f!(residuals::AbstractVector{Float64}, x::AbstractVector{Float64})
+        residuals .= get_static_residuals!(ws, w.params, x, exogenous, m)
+    end
+    
+    residuals = zeros(m.endogenous_nbr)
+    f!(residuals, x0)
+    
+    df = OnceDifferentiable(f!, J!, vec(x0), residuals, A0)
+    f!(residuals, x0 .+ 0.1)
+    result = nlsolve(df, x0::Vector{Float64}, show_trace=false)
     if converged(result)
         results.trends.endogenous_steady_state .= result.zero
     else
