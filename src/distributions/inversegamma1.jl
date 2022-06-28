789c4c9f57701251fccc6b5bf1c7723652c1a529
MichelJuillard <michel.juillard@mjui.fr>
2022-06-28 10:55:57 +0200

    add Inverse Gamma type 1 disrtibution

--- src/distributions/inversegamma1.jl
+++ src/distributions/inversegamma1.jl
@@ -1,3 +1,7 @@
+using Distributions
+import Distributions: zval, cdf, ccdf, logcdf, logccdf, quantile, cquantile, invlogcdf, invlogccdf, rand, params, shape, scale, rate, pdf, logpdf 
+using Random
+using SpecialFunctions
 """
     InverseGamma1(α, θ)
 
@@ -5,11 +9,11 @@ The *inverse Gamma distribution* with shape parameter `α` and scale `θ` has pr
 density function
 
 ```math
-f(x; \\alpha, \\theta) = \\frac{\\theta^\\alpha x^{-(\\alpha + 1)}}{\\Gamma(\\alpha)}
-e^{-\\frac{\\theta}{x}}, \\quad x > 0
+f(x; \\alpha, \\theta) = 2\\frac{\\theta^\\alpha x^{-(2\\alpha + 1)}}{\\Gamma(\\alpha)}
+e^{-\\frac{\\theta}{x^2}}, \\quad x > 0
 ```
 
-It is related to the [`Gamma`](@ref) distribution: if ``X \\sim \\operatorname{Gamma}(\\alpha, \\beta)``, then ``1 / \\sqrt{X} \\sim \\operatorname{InverseGamma1}(\\alpha, \\beta^{-1})``.
+It is related to the [`Gamma`](@ref) distribution: if ``X \\sim \\operatorname{Gamma}(\\alpha, \\beta)``, then ``1 / X \\sim \\operatorname{InverseGamma1}(\\alpha, \\beta^{-1})``.
 
 ```julia
 InverseGamma1()        # Inverse Gamma distribution with unit shape and unit scale, i.e. InverseGamma1(1, 1)
@@ -25,14 +29,14 @@ External links
 
 * [Inverse gamma distribution on Wikipedia](http://en.wikipedia.org/wiki/Inverse-gamma_distribution)
 """
-struct InverseGamma1{T<:Real} <: ContinuousUnivariateDistribution
-    invd::Gamma{T}
+struct InverseGamma1{T<:Real} <: Distributions.ContinuousUnivariateDistribution
+    α::T
     θ::T
-    InverseGamma1{T}(α::T, θ::T) where {T<:Real} = new{T}(Gamma(α, inv(θ), check_args=false), θ)
+    InverseGamma1{T}(α::T, θ::T) where {T<:Real} = new{T}(α, θ)
 end
 
 function InverseGamma1(α::T, θ::T; check_args::Bool=true) where {T <: Real}
-    @check_args InverseGamma1 (α, α > zero(α)) (θ, θ > zero(θ))
+    @Distributions.check_args InverseGamma1 (α, α > zero(α)) (θ, θ > zero(θ))
     return InverseGamma1{T}(α, θ)
 end
 
@@ -41,7 +45,7 @@ InverseGamma1(α::Integer, θ::Integer; check_args::Bool=true) = InverseGamma1(f
 InverseGamma1(α::Real; check_args::Bool=true) = InverseGamma1(α, one(α); check_args=check_args)
 InverseGamma1() = InverseGamma1{Float64}(1.0, 1.0)
 
-@distr_support InverseGamma1 0.0 Inf
+@Distributions.distr_support InverseGamma1 0.0 Inf
 
 #### Conversions
 convert(::Type{InverseGamma1{T}}, α::S, θ::S) where {T <: Real, S <: Real} = InverseGamma1(T(α), T(θ))
@@ -52,9 +56,9 @@ Base.convert(::Type{InverseGamma1{T}}, d::InverseGamma1{T}) where {T<:Real} = d
 
 #### Parameters
 
-shape(d::InverseGamma1) = shape(d.invd)
+shape(d::InverseGamma1) = d.α
 scale(d::InverseGamma1) = d.θ
-rate(d::InverseGamma1) = scale(d.invd)
+rate(d::InverseGamma1) = inv(shape(d))
 
 params(d::InverseGamma1) = (shape(d), scale(d))
 partype(::InverseGamma1{T}) where {T} = T
@@ -62,66 +66,66 @@ partype(::InverseGamma1{T}) where {T} = T
 
 #### Parameters
 
-mean(d::InverseGamma1{T}) where {T} = ((α, θ) = params(d); α  > 1 ? θ / (α - 1) : T(Inf))
+mean(d::InverseGamma1{T}) where {T} = ((α, θ) = params(d); α  > 1/2 ? sqrt(1/θ)*gamm(α - 1/2)/gamm(α)  : T(Inf))
 
-mode(d::InverseGamma1) = scale(d) / (shape(d) + 1)
+#mode(d::InverseGamma1) = scale(d) / (shape(d) + 1)
 
 function var(d::InverseGamma1{T}) where T<:Real
     (α, θ) = params(d)
     α > 2 ? θ^2 / ((α - 1)^2 * (α - 2)) : T(Inf)
 end
 
-function skewness(d::InverseGamma1{T}) where T<:Real
-    α = shape(d)
-    α > 3 ? 4sqrt(α - 2) / (α - 3) : T(NaN)
-end
+#function skewness(d::InverseGamma1{T}) where T<:Real
+#    α = shape(d)
+#    α > 3 ? 4sqrt(α - 2) / (α - 3) : T(NaN)
+#end
 
-function kurtosis(d::InverseGamma1{T}) where T<:Real
-    α = shape(d)
-    α > 4 ? (30α - 66) / ((α - 3) * (α - 4)) : T(NaN)
-end
+#function kurtosis(d::InverseGamma1{T}) where T<:Real
+#    α = shape(d)
+#    α > 4 ? (30α - 66) / ((α - 3) * (α - 4)) : T(NaN)
+#end
 
-function entropy(d::InverseGamma1)
-    (α, θ) = params(d)
-    α + loggamma(α) - (1 + α) * digamma(α) + log(θ)
-end
+#function entropy(d::InverseGamma1)
+#    (α, θ) = params(d)
+#    α + loggamma(α) - (1 + α) * digamma(α) + log(θ)
+#end
 
-function kldivergence(p::InverseGamma1, q::InverseGamma1)
-    # We can reuse the implementation of Gamma
-    return kldivergence(p.invd, q.invd)
-end
+#function kldivergence(p::InverseGamma1, q::InverseGamma1)
+#    # We can reuse the implementation of Gamma
+#    return kldivergence(p.invd, q.invd)
+#end
 
 
 #### Evaluation
 
 function logpdf(d::InverseGamma1, x::Real)
     (α, θ) = params(d)
-    log(2) + α * log(θ) - loggamma(α) - (0.5*α + 1) * log(x) - θ / (x .* x)
+    log(2) + α * log(θ) - loggamma(α) - (2 * α + 1) * log(x) - θ / (x * x)
 end
 
-function zval(::InverseGamma1, x::Real) = inv(sqrt(max(x, 0)))
+zval(::InverseGamma1, x::Real) = sqrt(max(x, 0))
 
-cdf(d::InverseGamma1, x::Real) = ccdf(d.invd, zval(d, x))
-ccdf(d::InverseGamma1, x::Real) = cdf(d.invd, zval(d, x))
-logcdf(d::InverseGamma1, x::Real) = logccdf(d.invd, zval(d, x))
-logccdf(d::InverseGamma1, x::Real) = logcdf(d.invd, zval(d, x))
+cdf(d::InverseGamma1, x::Real) = cdf(InverseGamma(d.α, d.θ), zval(d, x))
+ccdf(d::InverseGamma1, x::Real) = ccdf(InverseGamma(d.α, d.θ), zval(d. x))
+logcdf(d::InverseGamma1, x::Real) = logcdf(InverseGamma(d.α, d.θ), zval(d. x))
+logccdf(d::InverseGamma1, x::Real) = logccdf(InverseGamma(d.α, d.θ), zval(d. x))
 
-quantile(d::InverseGamma1, p::Real) = inv(cquantile(d.invd, p))
-cquantile(d::InverseGamma1, p::Real) = inv(quantile(d.invd, p))
-invlogcdf(d::InverseGamma1, p::Real) = inv(invlogccdf(d.invd, p))
-invlogccdf(d::InverseGamma1, p::Real) = inv(invlogcdf(d.invd, p))
+quantile(d::InverseGamma1, p::Real) = sqrt(quantile(InversGamme(d.α, d.θ), p))
+cquantile(d::InverseGamma1, p::Real) = sqrt(cquantile(InversGamme(d.α, d.θ), p))
+invlogcdf(d::InverseGamma1, p::Real) = sqrt(invlogcdf(InversGamme(d.α, d.θ), p))
+invlogccdf(d::InverseGamma1, p::Real) = sqrt(invlogccdf(InversGamme(d.α, d.θ), p))
 
-function mgf(d::InverseGamma1{T}, t::Real) where T<:Real
-    (a, b) = params(d)
-    t == zero(t) ? one(T) : 2(-b*t)^(0.5a) / gamma(a) * besselk(a, sqrt(-4*b*t))
-end
+#function mgf(d::InverseGamma1{T}, t::Real) where T<:Real
+#    (a, b) = params(d)
+#    t == zero(t) ? one(T) : 2(-b*t)^(0.5a) / gamma(a) * besselk(a, sqrt(-4*b*t))
+#end
 
-function cf(d::InverseGamma1{T}, t::Real) where T<:Real
-    (a, b) = params(d)
-    t == zero(t) ? one(T)+zero(T)*im : 2(-im*b*t)^(0.5a) / gamma(a) * besselk(a, sqrt(-4*im*b*t))
-end
+#function cf(d::InverseGamma1{T}, t::Real) where T<:Real
+#    (a, b) = params(d)
+#    t == zero(t) ? one(T)+zero(T)*im : 2(-im*b*t)^(0.5a) / gamma(a) * besselk(a, sqrt(-4*im*b*t))
+#end
 
 
 #### Evaluation
 
-rand(rng::AbstractRNG, d::InverseGamma1) = 1 / sqrt(rand(rng, d.invd))
+rand(rng::AbstractRNG, d::InverseGamma1) = sqrt(rand(rng, InverseGamma(d.α, d.θ)))
