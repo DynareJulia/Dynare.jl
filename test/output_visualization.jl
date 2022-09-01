a21762ddecc447bee88a2ecb923dfbcdf41d166b
Satya Bade <satya@quantum-machines.co>
2022-09-01 18:02:27 +0200

    To displaying posterior mode if computed + add seperate function to plot priors

--- test/output_visualization.jl
+++ test/output_visualization.jl
@@ -5,7 +5,6 @@ using KernelDensity
 using Distributions
 
 context = @dynare "test/models/ls2003/ls2003.mod"
-chains = mh_estimation(context, datafile = "test/models/ls2003/data_ca1.csv", first_obs=8, last_obs=86, iterations = 10000)
 
 function find(names::Vector, s)
     for (i, n) in enumerate(names)
@@ -15,28 +14,65 @@ function find(names::Vector, s)
     end
 end
 
-function mcmc_diagnostics(chains, context, names)
+has_posterior_mode(context) = length(context.work.estimated_parameters.posterior_mode)>0
 
+function mcmc_diagnostics(chains, context, names)
     indices = [find(context.work.estimated_parameters.name, e) for e in names]
-    posterior_pdfs = []
     prior_pdfs = []
     n_plots = length(indices)
-    x_axis = []
-
+    posterior_pdfs = []
+    posterior_x_axes = []
+    prior_x_axes = []
     for i in indices
         U = kde(chains.value.data[:, i])
+        prior = context.work.estimated_parameters.prior[i]
+        m, v = mean(prior), var(prior)
+        prior_x_axis = LinRange(m-15*v, m+15*v, length(U.x))
+        prior_pdf = [pdf(prior, e) for e in prior_x_axis]
         push!(posterior_pdfs, U.density*(U.x[2] - U.x[1]))
-        prior_pdf = [pdf(context.work.estimated_parameters.prior[i], e) for e in U.x]
-        push!(x_axis, U.x)
+        push!(posterior_x_axes, U.x)
         push!(prior_pdfs, prior_pdf/sum(prior_pdf))
+        push!(prior_x_axes, prior_x_axis)
     end
 
     posterior_pdfs = hcat(posterior_pdfs...)
+    posterior_x_axes = hcat(posterior_x_axes...)
     prior_pdfs = hcat(prior_pdfs...)
-    x_axis = hcat(x_axis...)
+    prior_x_axes = hcat(prior_x_axes...)
     names = hcat(names...)
-    f = plot(x_axis, prior_pdfs, layout=n_plots, title=names, linecolor=:darkgrey, legend=false, linewidth=3)
-    plot!(f, x_axis, posterior_pdfs, layout=n_plots, linecolor=:darkblue, legend=false, linewidth=3)
+
+    f = plot(posterior_x_axes, posterior_pdfs, layout=n_plots, linecolor=:darkblue, legend=false, linewidth=3)
+    plot!(f, prior_x_axes, prior_pdfs, layout=n_plots, title=names, linecolor=:darkgrey, legend=false, linewidth=3)
+    if has_posterior_mode(context)
+        posterior_modes = [context.work.estimated_parameters.posterior_mode[i] for i in indices]
+        plot!(f, hcat(posterior_modes...), layout=n_plots, seriestype=:vline, line=(:dot,3), linecolor=:green, legend=false, linewidth=3)
+    end
+    f
 end
 
-mcmc_diagnostics(chains, context, ["psi1", "psi2", "rr", "tau"])
+
+function plot_priors(context, names, n_points = 100)
+    indices = [find(context.work.estimated_parameters.name, e) for e in names]
+    prior_pdfs = []
+    n_plots = length(indices)
+    prior_x_axes = []
+    for i in indices
+        prior = context.work.estimated_parameters.prior[i]
+        m, v = mean(prior), var(prior)
+        prior_x_axis = LinRange(m-15*v, m+15*v, n_points)
+        prior_pdf = [pdf(prior, e) for e in prior_x_axis]
+        push!(prior_pdfs, prior_pdf/sum(prior_pdf))
+        push!(prior_x_axes, prior_x_axis)
+    end
+    prior_pdfs = hcat(prior_pdfs...)
+    prior_x_axes = hcat(prior_x_axes...)
+    names = hcat(names...)
+    f = plot(prior_x_axes, prior_pdfs, layout=n_plots, title=names, linecolor=:darkgrey, legend=false, linewidth=3)
+    f
+end
+
+
+f = mcmc_diagnostics(Dynare.chains, context, ["psi1", "psi2", "psi3", "rho_R"])
+display(f)
+g = plot_priors(context, ["psi1", "psi2", "psi3", "rho_R"])
+display(g)
