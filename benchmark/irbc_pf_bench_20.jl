69df9902bd6803923c825c012ccd66bfbfe97554
MichelJuillard <michel.juillard@mjui.fr>
2022-10-29 17:36:25 +0200

    updating benchmarks

--- benchmark/irbc_pf_bench_20.jl
+++ benchmark/irbc_pf_bench_20.jl
@@ -7,10 +7,14 @@ context = @dynare "../test/models/irbc/irbc_pf" "-DN=20"
 periods = 300
 
 m = context.models[1]
-ddf = context.dynarefunctions
 ncol = m.n_bkwrd + m.n_current + m.n_fwrd + 2 * m.n_both
-tmp_nbr = ddf.dynamic!.tmp_nbr::Vector{Int64}
-dynamic_ws = Dynare.DynamicWs(m.endogenous_nbr, m.exogenous_nbr, ncol, sum(tmp_nbr[1:2]))
+tmp_nbr = m.dynamic_tmp_nbr::Vector{Int64}
+dynamic_ws = Dynare.DynamicWs(m.endogenous_nbr,
+                              m.exogenous_nbr,
+                              sum(tmp_nbr[1:2]),
+                              m.dynamic_g1_sparse_colptr,
+                              m.dynamic_g1_sparse_rowval
+                              )
 perfect_foresight_ws = Dynare.PerfectForesightWs(context, periods)
 X = perfect_foresight_ws.shocks
 initialvalues = Dynare.get_dynamic_initialvalues(context)
@@ -33,9 +37,11 @@ steadystate = results.trends.endogenous_steady_state
 terminalvalues = view(guess_values, :, periods)
 params = work.params
 JJ = perfect_foresight_ws.J
-
 exogenous = perfect_foresight_ws.x
+nzval = similar(m.dynamic_g1_sparse_rowval, Float64)
+nzval1 = similar(nzval)
 
+#=
 ws_threaded = [
     Dynare.DynamicWs(
         m.endogenous_nbr,
@@ -44,6 +50,7 @@ ws_threaded = [
         length(temp_vec),
     ) for i = 1:Threads.nthreads()
 ]
+=#
 
 function f!(residuals, y)
     @debug "$(now()): start f!"
@@ -57,7 +64,6 @@ function f!(residuals, y)
         steadystate,
         params,
         m,
-        ddf,
         periods,
         temp_vec,
     )
@@ -69,15 +75,25 @@ function J!(
     y::AbstractVecOrMat{Float64},
 )
     @debug "$(now()): start J!"
-    A = Dynare.makeJacobian!(
+    A = Dynare.updateJacobian!(
         JJ,
+        Dynare.DFunctions.dynamic_derivatives!,
         vec(y),
         initialvalues,
         terminalvalues,
+        dynamic_variables,
         exogenous,
-        context,
         periods,
-        ws_threaded,
+        temp_vec,
+        params,
+        steadystate,
+        m.dynamic_g1_sparse_colptr,
+        nzval,
+        m.endogenous_nbr,
+        m.exogenous_nbr,
+        perfect_foresight_ws.permutations,
+        nzval1
+        #        ws_threaded,
     )
     @debug count(!iszero, A) / prod(size(A))
     @debug "$(now()): end J!"
@@ -88,6 +104,7 @@ function fj!(residuals, JJ, y)
     J!(JJ, vec(y))
 end
 
+#=
 @debug "$(now()): start makeJacobian"
 A0 = Dynare.makeJacobian!(
     JJ,
@@ -100,17 +117,18 @@ A0 = Dynare.makeJacobian!(
     ws_threaded,
 )
 @debug "$(now()): end makeJacobian"
+=#
 @debug "$(now()): start f!"
 f!(residuals, vec(guess_values))
 @debug "$(now()): end f!"
 @debug "$(now()): start J!"
-J!(A0, guess_values)
+J!(JJ, guess_values)
 @debug "$(now()): end J!"
-df = Dynare.OnceDifferentiable(f!, J!, vec(guess_values), residuals, A0)
+df = Dynare.OnceDifferentiable(f!, J!, vec(guess_values), residuals, JJ)
 @debug "$(now()): start nlsolve"
 
 rr = copy(residuals)
-F = Dynare.lu(A0)
+F = Dynare.lu(JJ)
 b = @benchmark res = Dynare.nlsolve(df, vec(guess_values), method = :robust_trust_region, show_trace = false, ftol=cbrt(eps()));
 
 
