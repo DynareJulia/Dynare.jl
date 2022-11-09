d9ec77ed0a0210e85cefd18310aea6831aeef851
Satya Bade <satya@quantum-machines.co>
2022-11-09 21:06:15 +0100

    use os specific extension for sysimage

--- precompile.jl
+++ precompile.jl
@@ -1,2 +1,11 @@
 using PackageCompiler, Dynare
-create_sysimage(:Dynare, sysimage_path="sys_dynare.so", precompile_execution_file="precompile_Dynare.jl")
+
+sysimage_path = "sys_dynare.so"
+
+if Sys.iswindows()
+    sysimage_path = "sys_dynare.dll"
+elseif Sys.isapple()
+    sysimage_path = "sys_dynare.dylib"
+end
+    
+create_sysimage(:Dynare, sysimage_path=sysimage_path, precompile_execution_file="precompile_Dynare.jl")
