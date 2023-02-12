d23a165033806f85a8f340b52352477ac741d0ab
MichelJuillard <michel.juillard@mjui.fr>
2023-02-12 21:29:39 +0100

    Revising test block decomposition

--- test/test_block_decomposition.jl
+++ test/test_block_decomposition.jl
@@ -17,10 +17,19 @@ J = Dynare.get_dynamic_jacobian!(ws, params, endogenous, exogenous, steadystate,
 #Incidence matrix
 using SparseArrays
 T=Vector{Bool}(undef, length(J.rowval))
+# all T elements must be true
+fill!(T, true)
 Incidence = SparseMatrixCSC(size(J, 1), size(J,2), J.colptr, J.rowval, T)
 
 #Model normalization
-for n in 1:length(steadystate)
-    U = BitMatrix(hcat(Incidence[:,n+1:2*n], Incidence[:,2n+1:3*n])) #bipartite graph
-    matching, matched = findmaxcardinalitybipartitematching(U) #maximum cardinality matching of the graph
-end
+n = model.endogenous_nbr
+U = BitMatrix(Incidence[:,n+1:2*n] .| Incidence[:,2n+1:3*n]) #bipartite graph
+matching, matched = findmaxcardinalitybipartitematching(U) #maximum cardinality matching of the graph
+# (Dict(5 => 4, 4 => 3, 6 => 6, 2 => 2, 3 => 5, 1 => 1)
+# Equation 1 determines variable 1
+# Equation 2 determines variable 3
+# Equation 3 determines variable 5
+# Equation 4 determines variable 3
+# Equation 5 determines variable 4
+# Equation 6 determines variable 6
+!all(matched) && error("Model can't be normalized")
