0a0439500c1f70a67422a84c81c629e048b0a1b2
FOJ-0 <fordonez@fen.uchile.cl>
2023-02-16 18:59:19 -0300

    update

--- test/test_block_decomposition.jl
+++ test/test_block_decomposition.jl
@@ -1,6 +1,7 @@
 using Dynare
 using SparseArrays
 using BipartiteMatching
+using Graphs
 
 context = @dynare "models/example1/example1.mod";
 
@@ -11,16 +12,23 @@ ws = Dynare.DynamicWs(context)
 params = context.work.params
 steadystate = results.trends.endogenous_steady_state
 endogenous = repeat(steadystate, 3)
-exogenous = results.trends.exogenous_steady_state # exogenous = repeat(exo_steadystate', 3)
+exogenous = results.trends.exogenous_steady_state 
 J = Dynare.get_dynamic_jacobian!(ws, params, endogenous, exogenous, steadystate, model, 200)
 
 #Incidence matrix
-using SparseArrays
-T=Vector{Bool}(undef, length(J.rowval))
+T=Vector{Bool}(undef, length(J.rowval)) # all T elements must be true
+fill!(T, true)
 Incidence = SparseMatrixCSC(size(J, 1), size(J,2), J.colptr, J.rowval, T)
 
 #Model normalization
-for n in 1:length(steadystate)
-    U = BitMatrix(hcat(Incidence[:,n+1:2*n], Incidence[:,2n+1:3*n])) #bipartite graph
-    matching, matched = findmaxcardinalitybipartitematching(U) #maximum cardinality matching of the graph
-end
+n = model.endogenous_nbr
+U = BitMatrix(Incidence[:,n+1:2*n] .| Incidence[:,2n+1:3*n]) #bipartite graph
+matching, matched = findmaxcardinalitybipartitematching(U) #maximum cardinality matching of the graph
+!all(matched) && error("Model can't be normalized")
+
+# Strongly connected components
+g = SimpleGraph(n) 
+edge_list = collect(matching)
+g = SimpleDiGraph(Edge.(edge_list))
+scc = strongly_connected_components(g)
+
