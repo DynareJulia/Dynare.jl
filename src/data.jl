bd589728ed9c20085090761173023abd0e2d4439
MichelJuillard <michel.juillard@mjui.fr>
2023-02-19 16:22:31 +0100

    complete switch to AxisArrayTables

--- src/data.jl
+++ src/data.jl
@@ -1,3 +1,4 @@
+using AxisArrayTables
 using ExtendedDates
 
 function find_letter_in_period(period::AbstractString)
@@ -9,88 +10,89 @@ function find_letter_in_period(period::AbstractString)
     return nothing
 end
 
-function identify_period_type(period::AbstractString)
-    c = find_letter_in_period(period)
-    let period_type
-        if isnothing(c)
-            if (cdash = count("-", period)) == 2
-                period_type = DaySE
-            elseif cdash == 1
-                period_type = MonthSE
-            elseif tryparse(Int64, period) !== nothing
-                period_type = Undated
-            else
-                throw(ErrorException)
-            end
+function identify_period_type(period::Union{AbstractString, Number})
+    if typeof(period) <: Number
+        if isinteger(period)
+            return Undated
         else
-            if c == 'Y'
-                period_type = YearSE
-            elseif c == 'A'
-                period_type = YearSE
-            elseif c == 'S'
-                period_type = ExtendedDates.SemesterSE
-            elseif c == 'H'
-                period_type = ExtendedDates.SemesterSE
-            elseif c == 'Q'
-                period_type = QuarterSE
-            elseif c == 'M'
-                period_type = MonthSE
-            elseif c == 'W'
-                period_type = WeekSE
-            elseif c == 'D'  # day of the year: 1980D364
-                period_type = DaySE
-            else
-                throw(ErrorException)
-            end
+            throw(ErrorException)
         end
-        return period_type
+    else
+        c = find_letter_in_period(period)
+        let period_type
+            if isnothing(c)
+                if (cdash = count("-", period)) == 2
+                    period_type = DaySE
+                elseif cdash == 1
+                    period_type = MonthSE
+                elseif tryparse(Int64, period) !== nothing
+                    period_type = Undated
+                else
+                    throw(ErrorException)
+                end
+            else
+                if c == 'Y'
+                    period_type = YearSE
+                elseif c == 'A'
+                    period_type = YearSE
+                elseif c == 'S'
+                    period_type = ExtendedDates.SemesterSE
+                elseif c == 'H'
+                    period_type = ExtendedDates.SemesterSE
+                elseif c == 'Q'
+                    period_type = QuarterSE
+                elseif c == 'M'
+                    period_type = MonthSE
+                elseif c == 'W'
+                    period_type = WeekSE
+                elseif c == 'D'  # day of the year: 1980D364
+                    period_type = DaySE
+                else
+                    throw(ErrorException)
+                end
+            end
+            return period_type
+        end
     end
 end
 
-function periodparse(period::AbstractString)::ExtendedDates.DatePeriod
+function periodparse(period::Union{AbstractString, Number})::ExtendedDates.PeriodsSinceEpoch
     period_type = identify_period_type(period)
     if period_type == YearSE
-        return parse(period_type, period, ExtendedDates.SimpleDateFormat("yY"))
+        return parse(period_type, period)
     elseif period_type == SemesterSE
-        return parse(period_type, period, ExtendedDates.SimpleDateFormat("ySs"))
+        return parse(period_type, period)
     elseif period_type == QuarterSE
-        return parse(period_type, period, ExtendedDates.SimpleDateFormat("yQq"))
+        return parse(period_type, period)
     elseif period_type == MonthSE
-        return parse(period_type, period, ExtendedDates.SimpleDateFormat("yMm"))
+        return parse(period_type, period)
     elseif period_type == WeekSE
-        return parse(period_type, period, ExtendedDates.SimpleDateFormat("yWw"))
+        return parse(period_type, period)
     elseif period_type == DaySE
-        return parse(period_type, period, ExtendedDates.SimpleDateFormat("y-m-d"))
+        return parse(period_type, period)
     elseif period_type == Undated
-        return parse(period_type, period, ExtendedDates.SimpleDateFormat("x"))
+        return Int(period)
     else
         throw(ErrorException)
     end
 end
 
-# To be moved to TimeDataFrames.jl
-function MyTimeDataFrame(filename)
-    df = DataFrame(CSV.File(filename))
-    local first_period::Any
-    periods = []
-    continuous = true
-    for name in names(df)
-        if uppercase(name) in ["DATE", "DATES", "PERIOD", "PERIODS"]
-            p = df[!, name]
-            foreach(x -> push!(periods, periodparse(x)), p)
-            if !is_continuous(periods)
-                continuous = false
-            end
-        else
-            periods = range(
-                UndatedDate(1),
-                length = DataFrames.nrow(df),
-                step = ExtendedDates.Undated(1),
-            )
+function MyAxisArrayTable(filename)
+    table = CSV.File(filename)
+    cols = AxisArrayTables.Tables.columnnames(table)
+    data = AxisArrayTables.Tables.matrix((;ntuple(i -> Symbol(i) => Tables.getcolumn(table, i), length(cols))...))
+    for (icol, name) in enumerate(cols)
+        if uppercase(String(name)) in ["DATE", "DATES", "PERIOD", "PERIODS", "TIME"]
+            rows = []
+            foreach(x -> push!(rows, periodparse(x)), data[:, icol])
+            k = union(1:icol-1, icol+1:size(data,2))
+            aat = AxisArrayTable(data[:, k], rows, cols[k])
+            return aat
         end
-        break
     end
-    return TimeDataFrame(df, periods, continuous)
+    rows = Undated(1):Undated(size(data,1))
+    aat = AxisArrayTable(data, rows, cols)
+    return aat
 end
 
 function is_continuous(periods::Vector{ExtendedDates.DatePeriod})
@@ -110,16 +112,15 @@ function get_data(
     start::Int64 = 1,
     last::Int64 = 0,
 )
-    tdf = MyTimeDataFrame(filename)
+    aat = MyAxisArrayTable(filename)
     ny = length(variables)
-
     if last == 0
-        last = size(tdf, 1) - start + 1
+        last = size(aat, 1) - start + 1
     end
     nobs = last - start + 1
     Y = Matrix{Union{Missing,Float64}}(undef, ny, nobs)
     for (i, v) in enumerate(variables)
-        Y[i, :] .= tdf[!, Symbol(v)][start:last]
+        Y[i, :] .= Matrix(aat[:, Symbol(v)])[start:last]
     end
     return Y
 end
