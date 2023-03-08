8902198562f00f6275932538544af7aadf91d3c0
MichelJuillard <michel.juillard@mjui.fr>
2023-03-08 12:54:51 +0100

    add block function example

--- test/test_block_decomposition.jl
+++ test/test_block_decomposition.jl
@@ -1,7 +1,12 @@
+module TestBlocks
+
 using Dynare
 using SparseArrays
 using BipartiteMatching
 using Graphs
+using RuntimeGeneratedFunctions
+
+RuntimeGeneratedFunctions.init(@__MODULE__)
 
 context = @dynare "models/example1/example1.mod";
 
@@ -33,3 +38,41 @@ iorder = [p[2] for p in sort(collect(pairs(matching)), by=x -> x[1])]
 g = SimpleDiGraph(U[:, iorder]) 
 scc = strongly_connected_components(g)
 
+<<<<<<< Updated upstream
+=======
+@show scc
+
+sdr = Dynare.DFunctions.SparseDynamicResid!
+
+equs = sdr.body.args[13].args[3].args
+
+sdr_block_1 = :((T, residual, y, x, params, steady_state) -> @inbounds begin; end)
+@show sdr_block_1.args
+
+function make_block_function(equs, block_component)
+    # make function template
+    func =  :((T, residual, y, x, params, steady_state) -> @inbounds begin; end)
+    # add equations belonging to this block
+    for (i, ie) in enumerate(block_component)
+        # in sdr each equations is the second of two elements
+        eq = copy(equs[2*ie])
+        # change the index of the residual
+        eq.args[1].args[2] = i
+        push!(func.args[2].args[2].args[3].args, eq)
+    end
+    return @RuntimeGeneratedFunction(func)
+end
+
+function make_blocks(func0, block_components)
+    block_functions = Function[]
+    equs = func0.body.args[13].args[3].args
+    for block in block_components
+        push!(block_functions, make_block_function(equs, block))
+    end
+    return block_functions
+end
+
+@show make_blocks(sdr, scc)
+
+end # end module          
+>>>>>>> Stashed changes
