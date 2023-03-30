3cbd42fd385b490b221162c2bd13cf7c7fe2533b
MichelJuillard <michel.juillard@mjui.fr>
2023-03-30 10:50:00 +0200

    test for analytical derivatives

---
+++ test/test_analytical_derivatives.jl
@@ -0,0 +1,9 @@
+using Dynare
+
+context = @dynare "models/analytical_derivatives/fs2000_sa.mod" "params_derivs_order=1";
+
+params = context.work.params
+steady_state = context.results.model_results[1].trends.endogenous_steady_state
+exogenous = context.results.model_results[1].trends.exogenous_steady_state
+
+(rp, gp) = Dynare.DFunctions.SparseStaticParametersDerivatives!(steady_state, exogenous, params)
