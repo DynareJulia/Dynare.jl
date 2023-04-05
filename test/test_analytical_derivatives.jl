162e645caa9352d468201b3a7595b665e70f1ccd
MichelJuillard <michel.juillard@mjui.fr>
2023-04-05 10:47:55 +0200

    update test_analytical_derivatives

--- test/test_analytical_derivatives.jl
+++ test/test_analytical_derivatives.jl
@@ -1,6 +1,6 @@
 using Dynare
 
-context = @dynare "models/analytical_derivatives/fs2000_sa.mod" "params_derivs_order=1";
+context = @dynare "models/analytical_derivatives/fs2000_sa.mod" "params_derivs_order=1" "notmpterms";
 
 params = context.work.params
 steady_state = context.results.model_results[1].trends.endogenous_steady_state
