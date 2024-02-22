4d37845c28ac9e642a67d6cb35e39ac666875246
MichelJuillard <michel.juillard@mjui.fr>
2024-02-22 19:14:27 +0100

    move NonLinearSolver type to mcp.jl

--- src/perfectforesight/perfectforesight.jl
+++ src/perfectforesight/perfectforesight.jl
@@ -28,11 +28,7 @@ function linear_solver!(::MumpsLS,
 end
 =#
 
-abstract type NonLinearSolver end
-struct PathNLS <: NonLinearSolver end
-struct DefaultNLS <: NonLinearSolver end
-
-mutable struct PerfectForesightOptions
+struct PerfectForesightOptions
     algo::PerfectForesightAlgo
     datafile::String
     display::Bool
@@ -193,37 +189,6 @@ struct PerfectForesightWs
 end
 
 include("conditional_simulation.jl")
-
-# Dummy definition for PathSolver extension
-function mcp_perfectforesight_core!(::DefaultNLS,
-                                    perfect_foresight_ws::PerfectForesightWs,
-                                    context::Dynare.Context,
-                                    periods::Int64,
-                                    guess_values::Vector{Float64},
-                                    initialvalues::Vector{Float64},
-                                    terminalvalues::Vector{Float64},
-                                    dynamic_ws::Dynare.DynamicWs;
-                                    maxit = maxit,
-                                    tolf = tolf,
-                                    tolx = tolx
-                                    )
-end
-
-function mcp_perfectforesight_core!(::DefaultNLS,
-                                    perfect_foresight_ws::PerfectForesightWs,
-                                    context::Dynare.Context,
-                                    periods::Int64,
-                                    guess_values::Vector{Float64},
-                                    initialvalues::Vector{Float64},
-                                    terminalvalues::Vector{Float64},
-                                    dynamic_ws::Dynare.DynamicWs,
-                                    flipinfo::FlipInformation,
-                                    infoperiod;
-                                    maxit= maxit,
-                                    tolf = tolf,
-                                    tolx = tolx
-                                    )
-end
 
 function perfect_foresight_setup!(context::Context, field=Dict{String, Any})
     periods = 0
