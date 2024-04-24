72f63db602ad548146d3c478225d90972c356e96
MichelJuillard <michel.juillard@mjui.fr>
2024-04-24 15:41:27 +0200

    remove debugging instruction

--- ext/PardisoSolver.jl
+++ ext/PardisoSolver.jl
@@ -5,8 +5,6 @@ using Dynare, MKL, Pardiso
 
 ps = MKLPardisoSolver()
 
-@show "PardisoSolver"
-
 Dynare.linear_solver!(::Dynare.PardisoLS, x::Vector{Float64}, A::SparseMatrixCSC{Float64, Int64}, b::Vector{Float64}) = Pardiso.solve!(ps, x, A, b)
 
 end
