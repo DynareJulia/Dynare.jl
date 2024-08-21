2201615e84f827d2562b7c4b7306c6002afd9a53
MichelJuillard <michel.juillard@mjui.fr>
2024-08-21 10:14:06 +0200

    adding multithreading for sparsegrids

--- test/test_sparsegrids.jl
+++ test/test_sparsegrids.jl
@@ -1,7 +1,7 @@
 using PATHSolver, Dynare
 using Test
 
-context = @dynare "models/global/irbc" "-DN=2" "-DMETHOD=\"NewtonRaphson()\"";
+context = @dynare "models/global/irbc" "-DN=2" "-DMETHOD=\"NewtonRaphson()\"" "stoponerror";
 context = @dynare "models/global/irbc" "-DN=2" "-DMETHOD=\"TrustRegion()\"";
 context = @dynare "models/global/irbc_irr" "-DN=2" "-DSOLVER=\"NLsolver\"";
 context = @dynare "models/global/irbc_irr" "-DN=2" "-DSOLVER=\"PATHSolver\"";
