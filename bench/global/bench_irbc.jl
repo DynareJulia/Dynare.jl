09308632151acc86b86bbbe9ad798e4ef1950248
MichelJuillard <michel.juillard@mjui.fr>
2024-08-25 14:50:38 +0200

    add benchmarks

---
+++ bench/global/bench_irbc.jl
@@ -0,0 +1,34 @@
+using CpuId
+using Dynare
+using Statistics
+
+function bench1(ncountries, depth)
+    context = dynare("irbc1", "-DN=$ncountries");
+    (grid, state_variables, policy_variables) = sparsegridapproximation(scaleCorrExclude=["lambda"], gridDepth = depth, maxRef = 0, tol_ti=0.001);
+    # skipping JIT compilation time of first iteration
+    return mean([t.value for (i, t) in enumerate(context.timings["sparsegrids"]) if i > 1])
+end
+
+function bench()
+    depth = 4
+    open("bench_irbc.md", "w+") do outfile
+        buffer = IOBuffer()
+        println(buffer, "# Dynare sparsegrid benchmark")
+        println(buffer, cpubrand(), cpucores())
+        println(buffer, "Number of threads: $(Threads.nthreads())")
+
+        try
+            ompthreads = ENV["OMP_NUM_THREADS"]
+            println(buffer, "Number of OMP threads: $ompthreads")
+        catch
+        end
+
+        for N in [2, 3 ] #4, 8, 12, 20]
+            m = bench1(N, depth)
+            println(buffer, "$N countries: mean iteration time: $m milliseconds")
+        end
+        write(outfile, String(take!(buffer)))
+    end
+end
+
+bench()
