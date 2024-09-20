2b9983c48578e3d9f1493e12a75d887815a28540
MichelJuillard <michel.juillard@mjui.fr>
2024-09-20 14:52:25 +0200

    update calls to compute_steady_state

--- src/perfectforesight/perfectforesight.jl
+++ src/perfectforesight/perfectforesight.jl
@@ -441,7 +441,7 @@ function get_dynamic_initialvalues(context::Context)
     else
         trends = context.results.model_results[1].trends
         if isempty(trends.endogenous_steady_state)
-            compute_steady_state!(context, Dict{String,Any}())
+            compute_steady_state!(context)
         end
         return trends.endogenous_steady_state
     end
@@ -459,7 +459,7 @@ function get_dynamic_terminalvalues(context::Context, periods::Int)
     else
         trends = context.results.model_results[1].trends
         if isempty(trends.endogenous_steady_state)
-            compute_steady_state!(context, Dict{String,Any}())
+            compute_steady_state!(context)
         end
         if modfileinfo.has_endval
             return trends.endogenous_terminal_steady_state
@@ -488,7 +488,7 @@ function perfect_foresight_initialization!(
     elseif algo == linearinterpolation
     elseif algo == steadystate 
         if isempty(trends.endogenous_steady_state)
-            compute_steady_state!(context, Dict{String,Any}())
+            compute_steady_state!(context)
         end
         if modfileinfo.has_endval
             x = trends.endogenous_terminal_steady_state
@@ -498,7 +498,7 @@ function perfect_foresight_initialization!(
         guess_values = repeat(x, periods)
     elseif algo == firstorder
         if isempty(trends.endogenous_steady_state)
-            compute_steady_state!(context, Dict{String,Any}())
+            compute_steady_state!(context)
         end
         y = simul_first_order!(context, periods, perfect_foresight_ws.x, dynamic_ws)
         guess_values = vec(y[1])
